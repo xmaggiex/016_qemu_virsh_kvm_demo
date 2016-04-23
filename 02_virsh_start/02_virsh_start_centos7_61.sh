80ea28bc8f251cad83f75f91a62009e5de51b0f9
jerry_0824 <63935127@qq.com>
2016-04-23 18:03:25 +0800

    Create 02_virsh_start_centos7_61.sh.

---
+++ 02_virsh_start/02_virsh_start_centos7_61.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#################################################
+# Filename: 02_virsh_start_centos7_61.sh
+# Author:   jerry_0824
+# Email:    63935127##qq.com
+# Date:     2016-04-23
+# Version:  v1.0.2
+#################################################
+
+# shutdown
+virsh shutdown centos7_61
+
+# virsh undefine
+# virsh undefine centos7_61
+
+# rm
+# rm -f ../img_files/centos7_61.qcow2
+
+# qemu-img create
+# qemu-img create -f qcow2 ../img_files/centos7_61.qcow2 5G
+
+# virsh define
+# virsh define 02_virsh_start_centos7_61.xml
+
+sleep 4s
+
+virsh start centos7_61
+
