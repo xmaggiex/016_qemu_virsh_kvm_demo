c7d0560d2eec60b5f8cdbe7eb84fc3c220267c34
jerry_0824 <63935127@qq.com>
2016-04-23 17:24:17 +0800

    Create 00_shell_cp_qcow2_files.sh

---
+++ 00_img_files/00_shell_cp_qcow2_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#################################################
+# Filename: 00_shell_cp_qcow2_files
+# Author:   jerry_0824
+# Email:    63935127##qq.com
+# Date:     2016-04-23
+# Version:  v1.0.1
+#################################################
+
+for((i = 62; i <= 70; ++i))
+do
+    echo "create file: centos7_$i.qcow2 ..."
+    cp -i centos7_61.qcow2 centos7_$i.qcow2
+    echo "create file: centos7_$i.qcow2 ok!"
+    echo ""
+    sleep 1
+done
