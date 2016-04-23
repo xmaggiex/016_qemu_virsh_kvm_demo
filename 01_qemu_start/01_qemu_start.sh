8cd5c7177e09d6dc19731507bc72aaf83c40b28c
jerry_0824 <63935127@qq.com>
2016-04-23 17:28:45 +0800

    Update 01_qemu_start.sh

--- 01_qemu_start/01_qemu_start.sh
+++ 01_qemu_start/01_qemu_start.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+#################################################
+# Filename: 01_qemu_start.sh
+# Author:   jerry_0824
+# Email:    63935127##qq.com
+# Date:     2016-04-23
+# Version:  v1.0.1
+#################################################
 
+$ sudo apt-get install qemu-utils
+$ qemu-img create -f qcow2 centos7_71.img 5G
+$ qemu-img create -f qcow2 centos7_71.qcow2 5G
+
+# 安装centos成功后
+# After install successfully
+$ sudo apt-get install qemu-system
+$ qemu-system-x86_64 ./img_files/centos7_71.img -m 512 -enable-kvm
+$ qemu-system-x86_64 ./img_files/centos7_71.img -m 256 -enable-kvm
